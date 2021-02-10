27f5058b82b4daa5e2480c8ef5ccd978d7554e44
Nabilfadilah <mohammadfadilah00@gmail.com>
2021-02-10 21:19:44 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,62 @@
+<?php 
+/* 
+Mohammad Nabil Fadilah
+203040020
+https://github.com/Nabilfadilah/pw2021_203040020
+Pertemuan 2 - 10 Februari 2021
+Mempelajari mengenai sintaks PHP 
+*/
+?>
+<?php 
+// standar Output 
+// echo, print 
+// print_r 
+// var_dump
+
+// penulisan sintaks PHP 
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel 
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka  
+// $nama = "Nabil Fadilah";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / % 
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat 
+// . 
+// $nama_depan = "Nabil";
+// $nama_belakang = "Fadilah";
+// echo $nama_depan . " " . $nama_belakang; 
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Nabil";
+// $nama .= " ";
+// $nama .= "Fadilah";
+// echo $nama;
+
+// Perbandingan 
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1"); 
+
+// identitas 
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika 
+// &&, ||, ! 
+// $x = 10;
+// var_dump($x < 20 || $x % 2 == 0); 
+
+?>
