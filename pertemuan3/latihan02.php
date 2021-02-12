8329580865a85f1f9fefb758c0661e9b56e7fe3c
Nabilfadilah <mohammadfadilah00@gmail.com>
2021-02-12 16:16:07 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan02.php
@@ -0,0 +1,25 @@
+<?php 
+/* 
+Mohammad Nabil Fadilah
+203040020
+https://github.com/Nabilfadilah/pw2021_203040020
+pertemuan 3 - 12 Februari 2021
+Mempelajari mengenai STRUKTUR KENDALI  
+*/
+?>
+<?php 
+// Pengkondisian / Percabangan
+// if else
+// if else if else 
+// ternary
+// switch
+
+$x = 2;
+if( $x < 20 ) {
+    echo "benar";
+} else if( $x == 20 ) {
+    echo "bingo!";    
+} else {
+    echo "salah"; 
+}
+?>
